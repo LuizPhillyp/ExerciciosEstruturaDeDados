19977dc95cb2ea688e32749aec75206b3e4029d3
Luiz Phillyp <luizphillyplg@gmail.com>
2022-05-11 18:21:20 -0300

    6:21 PM  - 5/11/2022

--- E03/source/main.c
+++ E03/source/main.c
@@ -7,14 +7,45 @@ int main(){
 
     for (int i = 0 ; i < 5; i++){
         for(int j = 0; j < 5; j++){
-           modificaElemento(minhaMatriz, i, j, 0);
+
+            modificaElemento(minhaMatriz, i, j, i * 5 +j);
         }
     }
 
-   imprimeMatriz(minhaMatriz);
-   printf("\n");
+    printf("Matriz01 -> %d X %d", recuperaNLinhas(minhaMatriz), recuperaNColunas(minhaMatriz));
+    printf("\n");
 
-   destroiMatriz(minhaMatriz);
+    imprimeMatriz(minhaMatriz);
+    printf("\n");
+
+    printf("\n");
+    printf("elemento [3][3] = %d", recuperaElemento(minhaMatriz, 3, 3));
+    printf("\n");
+
+    Matriz* minhaMatriz2 = inicializaMatriz(5, 5);
+
+    for (int i = 0 ; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if (i == j)
+                modificaElemento(minhaMatriz2, i, j, 10);
+            else
+                modificaElemento(minhaMatriz2, i, j, 0);
+        }
+    }
+
+    printf("\n");
+    printf("Matriz02 -> %d X %d", recuperaNLinhas(minhaMatriz2), recuperaNColunas(minhaMatriz2));
+    printf("\n");
+
+    imprimeMatriz(minhaMatriz2);
+    printf("\n");
+
+    printf("\n");
+    printf("elemento [3][3] = %d", recuperaElemento(minhaMatriz2, 1, 1));
+    printf("\n");
+
+    destroiMatriz(minhaMatriz2); 
+    destroiMatriz(minhaMatriz);
 
     return 0;
 }
--- E03/source/matriz.c
+++ E03/source/matriz.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "matriz.h"
 
+
+
+// Implementacao usando double-pointer
 struct matriz{
     
     int linhas;
@@ -11,10 +14,16 @@ struct matriz{
 
 Matriz* inicializaMatriz (int nlinhas, int ncolunas){
 
+    //checagem de condicoes
+    if (nlinhas <= 0 || ncolunas <=0){
+        printf("\n\n --ERRO: Inicializacao. O numero de linhas ou colunas da matriz deve ser um numero maior que zero \n\n");
+        exit(-1);
+    }
+
     // Alocacao do objeto "Matriz"
     Matriz* matriz_retorno = (Matriz*) malloc(sizeof(Matriz));
 
-    // Alocacao dos elementos da matriz
+    // Alocacao dos elementos da matriz (nesse caso, usando double-pointer)
     matriz_retorno -> matriz = (int**) malloc(nlinhas * sizeof(int*));
 
     for (int i = 0; i < nlinhas; i++){
@@ -31,10 +40,70 @@ Matriz* inicializaMatriz (int nlinhas, int ncolunas){
 }
 
 void modificaElemento (Matriz* mat, int linha, int coluna, int elem){
+
+    if(!mat){
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
+
+    if ((linha < 0 || linha >= (mat -> linhas)) || (coluna < 0 || coluna >= (mat -> colunas))){
+        printf("\n\n --ERRO: Modificacao. Tentativa de modificacao fora dos limites da matriz \n\n");
+        exit(-1);
+    }
+
     mat -> matriz[linha][coluna] = elem;
 }
 
+
+int recuperaElemento(Matriz* mat, int linha, int coluna){
+
+    
+    if(!mat){
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
+
+    if ((linha < 0 || linha >= (mat -> linhas)) || (coluna < 0 || coluna >= (mat -> colunas))){
+        printf("\n\n --ERRO: Acesso. Tentativa de acesso fora dos limites da matriz \n\n");
+        exit(-1);
+    }
+
+    return mat -> matriz[linha][coluna];
+}
+
+int recuperaNColunas (Matriz* mat){
+    
+    if(!mat){ 
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
+
+    return mat -> colunas;
+}
+
+    
+int recuperaNLinhas (Matriz* mat){
+
+    if(!mat){ 
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
+
+    return mat -> linhas;
+
+}
+
+Matriz* transposta (Matriz* mat){
+
+}
+
+
 void imprimeMatriz(Matriz* mat){
+
+    if(!mat){
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
 
     int nlinhas = mat -> linhas;
     int ncolunas = mat -> colunas;
@@ -44,7 +113,6 @@ void imprimeMatriz(Matriz* mat){
         
         // (1), (2) e (3) Tratam da formatacao da saida
 
-
         //---(1)
         if (i > 0)
             printf(" [");
@@ -53,7 +121,8 @@ void imprimeMatriz(Matriz* mat){
         
 
         for(int j = 0; j < ncolunas; j++){
-
+            
+            //impressao dos inteiros na matriz
             printf("%d", matriz[i][j]);
 
             // ---(2)
@@ -71,13 +140,19 @@ void imprimeMatriz(Matriz* mat){
 
 void destroiMatriz(Matriz* mat){
 
+    if(!mat){
+        printf("\n\n --ERRO: Nao foi possivel acessar o objero Matriz \n\n");
+        exit(-1);
+    }
+
+    // copiando em uma variavel local para melhor manipulacao
     int nlinhas = mat -> linhas;
     int ** matriz = mat -> matriz;
 
+    // liberacao de memoria na ordem inversa de alocacao
     for (int i = 0; i < nlinhas; i++){
         free(matriz[i]);  
     }
-
     free(matriz);
     free(mat);
 }
